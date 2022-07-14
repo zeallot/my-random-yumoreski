e8bdcb438a465fdff8d90179672ebdfa7a8f2793
vkaboskin <vkaboskin@aspirity.com>
2022-07-14 11:51:29 +0700

    add api express

---
+++ api.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const {mongoLink} = require('./config');
+const {getRandomJokeWithConnectedDb} = require('./mongoApi');
+const app = express()
+
+const host = '89.108.102.155'
+const port = 7000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/randomJoke', async (req, res) => {
+  const joke = await getRandomJokeWithConnectedDb();
+  res.json(joke);
+})
+
+mongoose.connect(mongoLink)
+  .then(async () => {
+    console.log('connected to mongo');
+    app.listen(port, host, () => {
+      console.log(`Server listens http://${host}:${port}`)
+    })
+  })
+  .catch(() => 'not connected');
